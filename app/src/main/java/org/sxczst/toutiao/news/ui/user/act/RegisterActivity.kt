bb3dc5b6fe07107548164248787b825cfa0cac1e
sxczst <sxczst@aliyun.com>
2020-11-14 23:28:50 +0800

    1. 验证手机号码逻辑

--- app/src/main/java/org/sxczst/toutiao/news/ui/user/act/RegisterActivity.kt
+++ app/src/main/java/org/sxczst/toutiao/news/ui/user/act/RegisterActivity.kt
@@ -1,19 +1,45 @@
 package org.sxczst.toutiao.news.ui.user.act
 
+import android.text.Editable
+import android.text.TextUtils
+import android.text.TextWatcher
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_register.*
 import org.sxczst.toutiao.news.R
 import org.sxczst.toutiao.news.base.BaseActivity
 import org.sxczst.toutiao.news.ui.user.presenter.RegisterPresenter
 import org.sxczst.toutiao.news.ui.user.view.RegisterView
+import org.sxczst.toutiao.news.utils.CommonUtils.checkMobile
+import org.sxczst.toutiao.news.utils.CommonUtils.replaceBlank
 import org.sxczst.toutiao.news.view.MyCheckBox
 
 /**
  * @Author      :sxczst
  * @Date        :Created in 2020/11/13 18:51
- * @Description :用户注册
+ * @Description :用户注册，TextWatcher 监听手机号码的改变
  */
-class RegisterActivity : BaseActivity<RegisterView, RegisterPresenter>(), RegisterView {
+class RegisterActivity : BaseActivity<RegisterView, RegisterPresenter>(), RegisterView,
+    TextWatcher {
+
+    /**
+     * 存取手机号信息
+     */
+    private var phoneText: String = ""
+
+    /**
+     * 校验电话号码
+     */
+    private fun checkPhone() {
+        // 手机长度，协议是否同意，手机号正则
+        if (phoneText.length == 13 && my_check_box.isChecked()
+            && checkMobile(replaceBlank(phoneText))
+        ) {
+            cv_next.setCardBackgroundColor(ContextCompat.getColor(this, R.color.c_e6645f))
+        } else {
+            cv_next.setCardBackgroundColor(ContextCompat.getColor(this, R.color.c_999999))
+        }
+    }
 
     override fun getLayoutId(): Int = R.layout.activity_register
 
@@ -21,10 +47,13 @@ class RegisterActivity : BaseActivity<RegisterView, RegisterPresenter>(), Regist
     }
 
     override fun initView() {
+
+        // 用户协议、隐私政策 复选框
         my_check_box.setOnClickListener {
             my_check_box.changeState()
+            checkPhone()
         }
-        // 用户协议、隐私政策的点击事件
+        // 用户协议、隐私政策的点击事件(自定义的回调接口)
         my_check_box.setClickableSpanText(object : MyCheckBox.ClickableSpanText {
             override fun onClick(type: Int) {
                 if (type == 1) {
@@ -36,6 +65,26 @@ class RegisterActivity : BaseActivity<RegisterView, RegisterPresenter>(), Regist
                 }
             }
         })
+
+        // 卡片按钮
+        cv_next.setCardBackgroundColor(ContextCompat.getColor(this, R.color.c_999999))
+        cv_next.setOnClickListener {
+            if (TextUtils.isEmpty(phoneText)) {
+                Toast.makeText(application, "请您输入手机号码", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (!my_check_box.isChecked()) {
+                Toast.makeText(application, "请您勾选用户协议", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            // TODO: 2020/11/14 跳转获取验证码
+        }
+
+        // 返回键
+        btn_back.setOnClickListener { finish() }
+
+        // 手机号码输入框
+        et_phone_number.addTextChangedListener(this)
     }
 
     override fun createPresenter(): RegisterPresenter? = RegisterPresenter()
@@ -44,5 +93,17 @@ class RegisterActivity : BaseActivity<RegisterView, RegisterPresenter>(), Regist
     }
 
     override fun setError(error: String) {
+    }
+
+    override fun afterTextChanged(s: Editable?) {
+    }
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+    }
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        // 手机号赋值
+        phoneText = s.toString()
+        checkPhone()
     }
 }
---
+++ app/src/main/java/org/sxczst/toutiao/news/utils/CommonUtils.kt
@@ -0,0 +1,27 @@
+package org.sxczst.toutiao.news.utils
+
+import java.util.regex.Pattern
+
+/**
+ * @Author      :sxczst
+ * @Date        :Created in 2020/11/14 22:59
+ * @Description :常用工具类
+ */
+object CommonUtils {
+    private const val regExp =
+        "/^(13[0-9]|14[01456879]|15[0-3,5-9]|16[2567]|17[0-8]|18[0-9]|19[0-3,5-9])\\d{8}\$/"
+
+    /**
+     * 手机号正则
+     */
+    fun checkMobile(mobile: String): Boolean = Pattern.compile(regExp).matcher(mobile).matches()
+
+    /**
+     * 去除空格
+     */
+    fun replaceBlank(str: String): String {
+        val compile = Pattern.compile("\\s*|\t|\r|\n")
+        val matcher = compile.matcher(str)
+        return matcher.replaceAll("")
+    }
+}
